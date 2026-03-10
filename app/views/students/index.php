903f7839d10276e11f41ff7d0c8f3e321280f890
WilsOnatt <wilshen.gouwesley@gmail.com>
2026-03-10 08:27:55 +0700

    wip: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -6,8 +6,42 @@
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
 </head>
-<body>
-    <h1 class="text-red-500">Daftar Siswa</h1>
-    <p>Menampilkan daftar siswa</p>
+
+<body class="min-h-screen flex flex-col bg-gray-100">
+    <!-- Header start -->
+    <header class="bg-blue-500 text-white">
+        <div class="flex items-center justify-between container mx-auto p-4">
+            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
+            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+        </div>
+    </header>
+    <!-- Header End -->
+
+    <!-- Main Content Start -->
+    <main class="grow container mx-auto p-4">
+        <div class="mt-8rem">
+            <!-- Card Header Start -->
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="text-xl font-bold">Daftar Siswa</h1>
+                <p>Menampilkan daftar siswa yang terdaftar</p>
+            </div>
+            <!-- Card Header End -->
+
+            <!-- Card Content Start -->
+             <div class="bg-white shadow rounded-lg">
+                
+             </div>
+            <!-- Card Content End -->
+        </div>
+    </main>
+    <!-- Main End -->
+
+    <!-- Footer Start -->
+    <footer class="bg-gray-800 text-white ">
+        <div class="text-center p-4">
+            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
+        </div>
+    </footer>
+    <!-- Footer End -->
 </body>
 </html>
